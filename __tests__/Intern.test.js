16922ada616e20d2a578f3091a0701cfa83e7072
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-03-01 00:58:41 -0600

    ran a test and it failed, fixed issue within inter.test.js; accidently switched the schools functions

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,13 +3,13 @@ const Intern = require('../lib/Intern');
 test ('Testing school input', () => {
     const intern = new Intern ('chris', 19, 'chris@test.com', 'Rice');
 
-    expect(intern.school()) .toEqual(expect.any(String));
+    expect(intern.school) .toEqual(expect.any(String));
 });
 
 test ('Testing school input working with getSchool()', () => {
     const intern = new Intern ('chris', 19, 'chris@test.com', 'Rice');
 
-    expect(intern.getSchool) .toEqual(expect.stringContaining(intern.school.toString()));
+    expect(intern.getSchool()) .toEqual(expect.stringContaining(intern.school.toString()));
 });
 
 test ('Testing role input', () => {
