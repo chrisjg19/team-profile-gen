bd626fe5ed555aec61f7e5fe788d21851036969e
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-03-01 18:26:54 -0600

    started prompt questions

--- index.js
+++ index.js
@@ -10,9 +10,34 @@ const Intern = require('./lib/Intern');
 
 
 //here will go the questions. probably will need to create 4. one for initial question and 3 for each type of employee
+init () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'Manager name?'
+    },
+    {
+        type: 'number',
+        name: 'id',
+        message: 'What is your ID number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'number',
+        name: 'officeNumber'
+        message: 'What is your office number?'
+    }
+])
+
+}
 
 //here will go the data to generate the employee cards
 
 
 
 
+init();
